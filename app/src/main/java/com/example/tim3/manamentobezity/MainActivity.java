55c3f8c7062d9dcbba6ed661a1b5bb93c48e448d
AS <xskachova@stuba.sk>
2019-01-10 09:28:10 +0100

    add aplication

--- app/src/main/java/com/example/tim3/manamentobezity/MainActivity.java
+++ app/src/main/java/com/example/tim3/manamentobezity/MainActivity.java
@@ -1,13 +1,216 @@
 package com.example.tim3.manamentobezity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    // statické globálne premenné pre prácu s údajmi
+    public static String NAME_KEY = "nameKey";
+    public static String SURNAME_KEY = "surnameKey";
+    public static String GENDER_KEY = "muž";
+    public static String EMAIL_KEY = "emailKey";
+    public static String AGE_KEY = "ageKey";
+    public static String LOCATION_KEY = "locationKey";
+    public static Boolean genderChecked = true;
+
+    // todo: co je to bundle ?
+    public static Bundle NAME = new Bundle();
+    public static Bundle SURNAME = new Bundle();
+    public static Bundle GENDER = new Bundle();
+    public static Bundle EMAIL = new Bundle();
+    public static Bundle AGE = new Bundle();
+    public static Bundle LOCATION = new Bundle();
+
+    // todo: lol pokus omyl - naco to je ???!
+    // práca s radio buttons
+    private RadioGroup radioGroup;
+    //private Button btnDisplay;
+    public String gender = "muž";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerRadioButton();
+        FillEmail();            // todo: je vobec potebne ??
+    }
+
+    // todo: je vobec potebne ??
+    private void FillEmail() {
+        EditText emailEditText = findViewById(R.id.Email);
+    }
+
+    // skontroluje správnosť zadaných údajov povinných parametrov
+    private Boolean IsValid(){
+        // todo komentár - na čo to je
+        EditText nameEditText = findViewById(R.id.Name);
+        EditText emailEditText = findViewById(R.id.Email);
+        EditText ageEditText = findViewById(R.id.Age);
+
+        // pomocné premenné na overenie jednotlivých zadaných údajov
+        boolean nameCorrect = false;
+        boolean emailCorrect = false;
+        boolean ageCorrect = false;
+        //boolean genderCorrect = false;
+
+        // skontroluj, či je správne zadané meno
+        if (nameEditText != null ){
+            String name =  nameEditText.getText().toString().trim();
+            // ak hodnota nie je zadaná
+            if  (name == null || name.isEmpty()){
+                // pop-up okno, ktoré upozorní, že meno je povinný atribút // todo ? spravny komentar
+                Toast toast1 = Toast.makeText(getApplicationContext(),
+                        "Musíte správne vyplniť údaj Meno, vyznačený hviezdičkou.",
+                        Toast.LENGTH_SHORT);
+                toast1.show();
+                // todo: co spravi funkcia request focus?
+                nameEditText.requestFocus();
+            }
+            else{
+                // textové pole s menom je správne vyplnené
+                nameCorrect = true;
+            }
+        }
+
+        // skontroluj, či je správne zadaný email
+        if (emailEditText != null ){
+            String email =  emailEditText.getText().toString().trim();
+            // ak hodnota nie je zadaná
+            if  (email == null || email.isEmpty() || !isValidEmail(email)){
+                // toast in android
+                Toast toast2 = Toast.makeText(getApplicationContext(),
+                        "Musíte správne vyplniť údaj Email, vyznačený hviezdičkou.",
+                        Toast.LENGTH_SHORT);
+                toast2.show();
+                emailEditText.requestFocus();
+            }
+            else{
+                emailCorrect = true;
+            }
+        }
+
+        // skontroluj, či je správne zadaný vek
+        if (ageEditText != null ){
+            String age =  ageEditText.getText().toString().trim();
+            if  (age == null || age.isEmpty() || !checkIfNumber(age)){
+                Toast toast3 = Toast.makeText(getApplicationContext(),
+                        "Musíte správne vyplniť údaj Vek (číslo), vyznačený hviezdičkou.",
+                        Toast.LENGTH_SHORT);
+                toast3.show();
+                ageEditText.requestFocus();
+            }
+            else{
+                ageCorrect = true;
+            }
+        }
+
+        // skontroluj, či je stlačený vybraný radiobutton
+        /*if(genderChecked ){
+            genderCorrect = true;
+        }*/
+
+        //if( nameCorrect && emailCorrect && ageCorrect && genderCorrect)
+
+        // over, či sú všetky hodnoty v textových poliach sú správne vyplnené
+        // a je vybraný radio button určujúci gender
+        if( nameCorrect && emailCorrect && ageCorrect && genderChecked)
+            return true;
+        else
+            return false;
+    }
+
+    // skontroluje či zadaný parameter je číslo
+    // je používaná aj v SecondActivity preto public static
+    public static boolean checkIfNumber(String age) {
+        try {
+            // int tmp = Integer.parseInt(str);
+            Integer.parseInt(age);
+        }
+        catch (NumberFormatException | NullPointerException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    // overí, či má email správnu formu - vyhovuje regulárnemu výrazu
+    // todo: ci public?
+    private boolean isValidEmail(String email) {
+        String EMAIL_REGEX = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+        Pattern pattern = Pattern.compile(EMAIL_REGEX, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    // funkcia otvorí dalšiu aktivitu (okno aplikácie) ak sú všetky parametre správne zadané
+    public void openNextActivity(View view){
+        if (IsValid()){
+            Intent intent = new Intent(this, SecondActivity.class);
+            FillIntent(intent);
+            startActivity(intent);
+        }
+    }
+
+    // funkcia vloží do statických premenných hodnoty z komponentov formuláru
+    private void FillIntent(Intent intent){
+        // meno
+        EditText nameEditText = findViewById(R.id.Name);
+        intent.putExtra(NAME_KEY, nameEditText.getText().toString());
+
+        // priezvisko
+        EditText surnameEditText = findViewById(R.id.Surname);
+        intent.putExtra(SURNAME_KEY, surnameEditText.getText().toString());
+
+        // email
+        EditText emailEditText = findViewById(R.id.Email);
+        intent.putExtra(EMAIL_KEY, emailEditText.getText().toString());
+
+        // vek
+        EditText ageEditText = findViewById(R.id.Age);
+        intent.putExtra(AGE_KEY, ageEditText.getText().toString());
+
+        // okres
+        EditText locationEditText = findViewById(R.id.Location);
+        intent.putExtra(LOCATION_KEY, locationEditText.getText().toString());
+
+        // todo: naco???  //na oklamanie static kontextu v dalsej aktivite
+        // todo: a co ked zvolim zenu???
+        MainActivity.GENDER.putString("gender", "" + gender);
+        MainActivity.NAME.putString("name",nameEditText.getText().toString());
+        MainActivity.SURNAME.putString("surname",surnameEditText.getText().toString());
+        MainActivity.LOCATION.putString("location",locationEditText.getText().toString());
+        MainActivity.EMAIL.putString("email",emailEditText.getText().toString());
+        MainActivity.AGE.putString("age",ageEditText.getText().toString());
+    }
+
+    // funkcia zistí, ktorý radio button je zaškrknutý
+    // todo: prepisat komentare
+    private void addListenerRadioButton() {
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup1);
+        Button btnDisplay = (Button) findViewById(R.id.Female);
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get selected radio button from radioGroup
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                // find the radiobutton by returned id
+                RadioButton radioButton = (RadioButton) findViewById(selectedId);
+                // todo: preco cislo a nie id radiobuttone?
+                if(selectedId == 2131165190)
+                    gender = "žena";
+                else
+                    gender = "muž";
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tim3/manamentobezity/SecondActivity.java
+++ app/src/main/java/com/example/tim3/manamentobezity/SecondActivity.java
@@ -1,13 +1,195 @@
 package com.example.tim3.manamentobezity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class SecondActivity extends AppCompatActivity {
+
+    // statické globálne premenné pre prácu s údajmi
+    public static String WEIGHT_KEY = "weightKey";
+    public static String SYSTOLIC_KEY = "0";
+    public static String DIASTOLIC_KEY = "1";
+    public static String HEARTBEATS_KEY = "heartbeatsKey";
+    public static String NOTES_KEY = "notesKey";
+    public static String NAME_KEY = "nameKey";
+    public static String SURNAME_KEY = "surnameKey";
+    public static String GENDER_KEY = "genderKey";
+    public static String EMAIL_KEY = "";
+    public static String AGE_KEY = "ageKey";
+    public static String LOCATION_KEY = "locationKey";
+
+    // premenné pre prácu s emailom
+    private String email;
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    // funkcia, ktorá vyplní todo: co spravi?
+    private void PrefillFormular(Intent intent){
+        // todo: naco tieto 2 riadky ?
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");      //chcem dostat formatovany datum
+        Calendar c = Calendar.getInstance();
+
+        //do statických premenných uloží údaje z minulej aktivity
+        SecondActivity.NAME_KEY = MainActivity.NAME.getString("name");       //do statickych premennych uklada udaje z min. aktivity
+        SecondActivity.SURNAME_KEY = MainActivity.SURNAME.getString("surname");
+        SecondActivity.GENDER_KEY = MainActivity.GENDER.getString("gender");
+        SecondActivity.EMAIL_KEY = MainActivity.EMAIL.getString("email");
+        SecondActivity.AGE_KEY = MainActivity.AGE.getString("age");
+        SecondActivity.LOCATION_KEY = MainActivity.LOCATION.getString("location");
+
+        // práca s mailom
+        email = MainActivity.EMAIL.getString("email");   // adresa, na ktorú sa správa odošle
+        // formát správy, ktorá obsahuje zadané údaje
+        message =   "Meno: " + SecondActivity.NAME_KEY + "\n" +
+                    "Priezvisko: " + SecondActivity.SURNAME_KEY + "\n" +
+                    "Pohlavie: " + SecondActivity.GENDER_KEY + "\n" +
+                    "Email: " + SecondActivity.EMAIL_KEY + "\n" +
+                    "Vek: " + SecondActivity.AGE_KEY + " rokov" + "\n" +
+                    "Okres: " + SecondActivity.LOCATION_KEY + "\n" +
+                    "Hmotnosť: " + intent.getStringExtra(SecondActivity.WEIGHT_KEY) + " kg" + "\n\n" +
+                    "Tep: " + intent.getStringExtra(SecondActivity.HEARTBEATS_KEY) + " bpm" + "\n" +
+                    "Systolický tlak: " + intent.getStringExtra(SecondActivity.SYSTOLIC_KEY) + " mmHg" + "\n" +
+                    "Diastolický tlak: " + intent.getStringExtra(SecondActivity.DIASTOLIC_KEY) + " mmHg" + "\n" +
+                    "Lieky: " + intent.getStringExtra(SecondActivity.NOTES_KEY) + "\n" + "\n" +
+                    "Dátum vyplnenia: " + Calendar.getInstance().getTime() + "\n"      //LocalDateTime.now()
+                    ;
+    }
+
+    // funkcia vloží do statických premenných hodnoty z komponentov formuláru
+    private void FillIntent(Intent intent){
+        // váha
+        EditText weightEditText = findViewById(R.id.Weight);
+        intent.putExtra(WEIGHT_KEY, weightEditText.getText().toString());
+
+        // systolický tlak
+        EditText systolicEditText = findViewById(R.id.Systolic);
+        intent.putExtra(SYSTOLIC_KEY, systolicEditText.getText().toString());
+
+        // diastolický tlak
+        EditText diastolicEditText = findViewById(R.id.Diastolic);
+        intent.putExtra(DIASTOLIC_KEY, diastolicEditText.getText().toString());
+
+        // počet úderov srdca
+        EditText heartbeatsEditText = findViewById(R.id.Heartbeats);
+        intent.putExtra(HEARTBEATS_KEY, heartbeatsEditText.getText().toString());
+
+        // poznámky o liekoch
+        EditText notesEditText = findViewById(R.id.Notes);
+        intent.putExtra(NOTES_KEY, notesEditText.getText().toString());
+    }
+
+    // skontroluje správnosť zadaných údajov povinných parametrov
+    private Boolean IsValid(){
+        // todo komentár - na čo to je
+        EditText weightEditText = findViewById(R.id.Weight);
+        EditText heartbeatsEditText = findViewById(R.id.Heartbeats);
+        EditText systolicEditText = findViewById(R.id.Systolic);
+        EditText diastolicEditText = findViewById(R.id.Diastolic);
+
+        // pomocné premenné na overenie jednotlivých zadaných údajov
+        boolean weightCorrect = false;
+        boolean heartbeatsCorrect = false;
+        boolean sysCorrect = false;
+        boolean diaCorrect = false;
+
+        // skontroluj, či je správne zadaná váha
+        if (weightEditText != null ){
+            String weight =  weightEditText.getText().toString().trim();
+            if  (weight == null || weight.isEmpty() || !MainActivity.checkIfNumber(weight)){
+                Toast toast1 = Toast.makeText(getApplicationContext(),
+                        "Musíte správne vyplniť údaj Hmotnosť (číslo), vyznačený hviezdičkou.",
+                        Toast.LENGTH_SHORT);
+                toast1.show();
+                weightEditText.requestFocus();
+            }
+            else{
+                weightCorrect = true;
+            }
+        }
+
+        // skontroluj, či je správne zadaný tep srdca
+        if (heartbeatsEditText != null ){
+            String heartbeats =  heartbeatsEditText.getText().toString().trim();
+            if  (heartbeats == null || heartbeats.isEmpty() || !MainActivity.checkIfNumber(heartbeats)){
+                Toast toast2 = Toast.makeText(getApplicationContext(),
+                        "Musíte správne vyplniť údaj Srdcový tep (číslo), vyznačený hviezdičkou.",
+                        Toast.LENGTH_SHORT);
+                toast2.show();
+                heartbeatsEditText.requestFocus();
+            }
+            else{
+                heartbeatsCorrect = true;
+            }
+        }
+
+        // skontroluj, či je správne zadaný systolický tlak
+        if (systolicEditText != null ){
+            String systolic =  systolicEditText.getText().toString().trim();
+            if  (systolic == null || systolic.isEmpty() || !MainActivity.checkIfNumber(systolic)){
+                Toast toast3 = Toast.makeText(getApplicationContext(),
+                        "Musíte správne vyplniť údaj Systolický tlak (číslo), vyznačený hviezdičkou.",
+                        Toast.LENGTH_SHORT);
+                toast3.show();
+                systolicEditText.requestFocus();
+            }
+            else{
+                sysCorrect = true;
+            }
+        }
+
+        // skontroluj, či je správne zadaný diastolický tlak
+        if (diastolicEditText != null ){
+            String diastolic =  diastolicEditText.getText().toString().trim();
+            if  (diastolic == null || diastolic.isEmpty() || !MainActivity.checkIfNumber(diastolic)){
+                Toast toast4 = Toast.makeText(getApplicationContext(),
+                        "Musíte správne vyplniť údaj Diastolický tlak (číslo), vyznačený hviezdičkou.",
+                        Toast.LENGTH_SHORT);
+                toast4.show();
+                diastolicEditText.requestFocus();
+            }
+            else{
+                diaCorrect = true;
+            }
+        }
+
+        // over, či sú všetky hodnoty v textových poliach sú správne vyplnené
+        if( weightCorrect && heartbeatsCorrect && sysCorrect && diaCorrect)
+            return true;
+        else
+            return false;
+    }
+
+    // funkcia ktorá odošle zadané údaje na zadaný email po stlačení tlačidla odoslať
+    public void sendForm(View view){
+        if (IsValid()){
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("message/rfc822");
+
+            FillIntent(i);
+            PrefillFormular(i);
+
+            // vytvorí email
+            i.putExtra(Intent.EXTRA_EMAIL  , new String[]{email});           // prijímateľ
+            i.putExtra(Intent.EXTRA_SUBJECT, "Zber dát ISZ 2018");      // predmet správy
+            i.putExtra(Intent.EXTRA_TEXT, message);                           // obsah správy
+
+            try {
+                startActivity(Intent.createChooser(i, "Send mail..."));
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(SecondActivity.this, "There are no email clients installed.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
